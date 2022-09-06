d6490fa3f6b95aabe9ad7213a672c3c3219ba9c8
The-CodeINN <67247138+The-CodeINN@users.noreply.github.com>
2022-09-06 12:59:03 +0000

    Learning the basics of typescript

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+var character = 'mario';
+console.log(character);
+// const inputs = document.querySelectorAll('input');
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+var cirum = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(cirum(7.5).toFixed(2));
---
+++ sandbox.ts
@@ -0,0 +1,14 @@
+const character = 'mario';
+
+console.log(character);
+
+// const inputs = document.querySelectorAll('input');
+
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+
+const cirum = (diameter: number) => {
+  return diameter * Math.PI;
+}
+console.log(cirum(7.5).toFixed(2));
