212099897efa6d6fe508293bc2f55729f4778b7a
The-CodeINN <67247138+The-CodeINN@users.noreply.github.com>
2022-09-09 23:21:07 +0000

    Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -35,3 +35,34 @@ let age = 25;
 let mixed = [];
 mixed.push(1);
 mixed.push("a");
+// Function
+let greet;
+greet = () => {
+    console.log('hello world');
+};
+// Function with specific type
+let greet2;
+greet2 = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
+};
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
+};
+const greetU = (user) => {
+    console.log(`${user.name} says hello`);
+};
+// Function Signatures
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action.trim() === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+let logDetail;
+logDetail = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
+logDetail({ name: 'David', age: 40 });
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -47,3 +47,48 @@ let age: any = 25;
 let mixed: any[] = [];
 mixed.push(1)
 mixed.push("a")
+
+// Function
+let greet: Function;
+greet = () => {
+  console.log('hello world')
+}
+
+// Function with specific type
+let greet2: (a: string, b: string) => void;
+greet2 = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`)
+}
+
+// Type Aliases
+type StringOrNum = string | number;
+type objWithName = { name: string, uid: StringOrNum };
+
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+}
+
+const greetU = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
+}
+
+// Function Signatures
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action.trim() === 'add') {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+}
+
+let logDetail: (obj: { name: string, age: number }) => void;
+
+type person = { name: string, age: number };
+
+logDetail = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+}
+
+logDetail({ name: 'David', age: 40 });
