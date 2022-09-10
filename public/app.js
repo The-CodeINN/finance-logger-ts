b3a53de02ad078bf6577ee4d26fd0973987e781a
The-CodeINN <67247138+The-CodeINN@users.noreply.github.com>
2022-09-10 00:29:05 +0000

    Switched to module system

--- public/app.js
+++ public/app.js
@@ -1,12 +1,5 @@
-"use strict";
-// const anchor = document.querySelector('a')!;
-// // if (anchor) {
-// //   console.log(anchor.href);
-// // }
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
+import { Invoice } from "./classes/invoice.js";
 const form = document.querySelector('.new-item-form');
-// console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
@@ -16,30 +9,11 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
-// Classes
-class Invoice {
-    // client: string;
-    // details: string;
-    // amount: number;
-    // constructor(c: string, d: string, a: number) {
-    //     this.client = c;
-    //     this.details = d;
-    //     this.amount = a;
-    // }
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invoices.forEach(inv => {
+invoices.forEach((inv) => {
     console.log(inv.client, inv.amount, inv.format());
 });
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,6 @@
-// const anchor = document.querySelector('a')!;
+import { Invoice } from "./classes/invoice.js";
 
-// // if (anchor) {
-// //   console.log(anchor.href);
-// // }
-
-// console.log(anchor.href);
-
-// const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -22,29 +14,6 @@ form.addEventListener('submit', (e: Event) => {
   console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
 
-// Classes
-class Invoice {
-    // client: string;
-    // details: string;
-    // amount: number;
-
-    // constructor(c: string, d: string, a: number) {
-    //     this.client = c;
-    //     this.details = d;
-    //     this.amount = a;
-    // }
-
-    constructor(
-        readonly client: string,
-        private details: string,
-        public amount: number
-    ) {}
-
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
-
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
@@ -52,6 +21,6 @@ let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.amount, inv.format());
 });
